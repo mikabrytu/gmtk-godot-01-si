130dad430f7fc13e4ea3323c42f196db105a69c5
mikabrytu <mikabrytu@gmail.com>
2024-05-18 12:34:23 -0300

    enemy group bounces left to right

--- scenes/enemy_group/enemy_group.gd
+++ scenes/enemy_group/enemy_group.gd
@@ -2,6 +2,16 @@ extends Node2D
 
 @export var step: int
 
+var direction = 1
+
 func _on_timer_timeout():
-	position.x += step
+	position.x += direction * step
+	
+
+func _on_screen_exited_left():
+	direction = 1
+	
+
+func _on_screen_exited_right():
+	direction = -1
 	
