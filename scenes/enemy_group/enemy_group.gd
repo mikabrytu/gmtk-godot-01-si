ea9b77033dda6c0d1d5017b50e5f06c1f52b1d15
mikabrytu <mikabrytu@gmail.com>
2024-05-18 12:47:23 -0300

    Spawn enemies in a grid

--- scenes/enemy_group/enemy_group.gd
+++ scenes/enemy_group/enemy_group.gd
@@ -1,8 +1,22 @@
 extends Node2D
 
+@export_category("Spawn Settings")
+@export var size: Vector2i
+@export var offset: Vector2
+@export var enemy: PackedScene
+
+@export_category("Movement")
 @export var step: int
 
 var direction = 1
+
+func _ready():
+	for x in size.x:
+		for y in size.y:
+			var clone = enemy.instantiate()
+			clone.position = Vector2(x * offset.x, y * offset.y)
+			add_child(clone)
+	
 
 func _on_timer_timeout():
 	position.x += direction * step
