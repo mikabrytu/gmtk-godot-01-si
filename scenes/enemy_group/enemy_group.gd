9449cee133bcf3a5434a46363d8441dfb22243c4
mikabrytu <mikabrytu@gmail.com>
2024-05-18 13:20:27 -0300

    Enemies advance toward player

--- scenes/enemy_group/enemy_group.gd
+++ scenes/enemy_group/enemy_group.gd
@@ -7,8 +7,14 @@ extends Node2D
 
 @export_category("Movement")
 @export var step: int
+@export var step_increase: int
+@export var timer_decrease: float
+@export var bounce_limit: int
+@export var change_threshold: int
 
 var direction = 1
+var kill_count = 0
+var bounce_count = 0
 
 func _ready():
 	for x in size.x:
@@ -19,18 +25,38 @@ func _ready():
 			add_child(clone)
 	
 
+func _increase_difficulty():
+	kill_count = 0
+	bounce_count = 0
+	
+	$Timer.wait_time -= timer_decrease
+	step += step_increase
+	bounce_limit -= 1
+	
+
+func _advance():
+	bounce_count += 1
+	if (bounce_count > bounce_limit):
+		position.y += step
+		bounce_count = 0
+	
+
 func _on_timer_timeout():
 	position.x += direction * step
 	
 
 func _on_screen_exited_left():
 	direction = 1
+	_advance()
 	
 
 func _on_screen_exited_right():
 	direction = -1
+	_advance()
 	
 
 func _on_enemy_killed():
-	print("Enemy is killed")
+	kill_count += 1
+	if (kill_count > change_threshold):
+		_increase_difficulty()
 	
